f2491f2a3ba136c7410694baa39fa1afb636193e
Dorababu Arigi <dorababuarigi765@gmail.com>
2022-05-09 20:25:16 +0530

    Adding the show  text in xml and kotlin for done button click functionality

--- app/src/main/java/com/example/mybio/MainActivity.kt
+++ app/src/main/java/com/example/mybio/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.mybio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.done_button).setOnClickListener{
+            showNickname(it)
+        }
+    }
+    private fun showNickname(v : View){
+        var showText=findViewById<TextView>(R.id.nickname_text);
+        var inputText=findViewById<EditText>(R.id.edit_nickname);
+        showText.text=inputText.text;
+        inputText.visibility= View.GONE;
+        v.visibility=View.GONE;
+        showText.visibility=View.VISIBLE;
+
     }
 }

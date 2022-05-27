9e7e6b19f56fef4fa3cf6ddfa47eef10b3a24982
Dorababu Arigi <dorababuarigi765@gmail.com>
2022-05-27 22:19:45 +0530

    Data binding for more fast access

--- app/src/main/java/com/example/mybio/MainActivity.kt
+++ app/src/main/java/com/example/mybio/MainActivity.kt
@@ -6,22 +6,29 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.mybio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener{
+//        setContentView(R.layout.activity_main)
+          binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+//        findViewById<Button>(R.id.done_button).setOnClickListener{
+//            showNickname(it)
+//        }
+        binding.doneButton.setOnClickListener{
             showNickname(it)
         }
     }
     private fun showNickname(v : View){
-        var showText=findViewById<TextView>(R.id.nickname_text);
-        var inputText=findViewById<EditText>(R.id.edit_nickname);
-        showText.text=inputText.text;
-        inputText.visibility= View.GONE;
-        v.visibility=View.GONE;
-        showText.visibility=View.VISIBLE;
+        binding.apply{
+            nicknameText.text=binding.editNickname.text
+            editNickname.visibility=View.GONE;
+            doneButton.visibility=View.GONE;
+            nicknameText.visibility=View.VISIBLE;
+        }
 
     }
 }
